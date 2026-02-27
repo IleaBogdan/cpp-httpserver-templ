042e11b2ba47a08d9a74c36044b979bff69705e1
IleaBogdan <ilea.bogdan06@gmail.com>
2026-02-27 22:48:13 +0200

    fixed the script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
-g++ -std=c++11 -pthread main.cpp -o myapp
+#!/bin/bash
+
+if [ ! -d "bin" ]; then
+    # echo "no bin dir"
+    mkdir bin
+fi
+
+g++ -std=c++23 -pthread main.cpp -o ./bin/server
 # If using SQLite:
-# g++ -std=c++11 -pthread main.cpp -lsqlite3 -o myapp
+# g++ -std=c++23 -pthread main.cpp -lsqlite3 -o myapp
--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <crow.h>
 #include <random>
 
+std::string get_random_number(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 100);
+    
+    return std::to_string(dis(gen));
+}
 int main() {
     crow::SimpleApp app;
     
-    CROW_ROUTE(app, "/random")([](){
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(1, 100);
-        
-        return std::to_string(dis(gen));
-    });
+    CROW_ROUTE(app, "/random").methods("GET"_method)(get_random_number);
     
     app.port(1337).multithreaded().run();
     return 0;
