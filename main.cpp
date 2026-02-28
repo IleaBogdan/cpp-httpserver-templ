dca3a824e36869f6b8e6e8613438a3f1ce59a4a7
IleaBogdan <ilea.bogdan06@gmail.com>
2026-02-28 14:05:37 +0200

    returning jsons

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <crow.h>
 #include <random>
+#include "json.hpp"
+using json=nlohmann::json;
 
-std::string get_random_number(){
+std::string get_random_number(void){
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
     
     return std::to_string(dis(gen));
+}
+json get_users_from_db(void){
+    return json({
+        {"Name","TEST_NAME"},
+        {"EMAIL","TEST_EMAIL"}
+    });
 }
 int main() {
     crow::SimpleApp app;
@@ -16,8 +24,15 @@ int main() {
     //     res.end();
     // });
     
-    CROW_ROUTE(app, "/")([](crow::request& req, crow::response& res) {
+    CROW_ROUTE(app, "/").methods("GET"_method)([](crow::request& req, crow::response& res) {
         res.redirect("/static/swagger/index.html");
+        res.end();
+    });
+
+    CROW_ROUTE(app,"/getUsers").methods("GET"_method)([](crow::request& req, crow::response& res){
+        auto data=get_users_from_db();
+        res.set_header("Content-Type", "application/json");
+        res.body=data.dump(); 
         res.end();
     });
     
