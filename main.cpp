07b4cbbd66af40bb2bec7a3fbe473745388a135a
IleaBogdan <ilea.bogdan06@gmail.com>
2026-02-28 15:51:29 +0200

    file downloading method

--- main.cpp
+++ main.cpp
@@ -69,6 +69,30 @@ signed main(){
         res.end();
     });
 
+    CROW_ROUTE(app, "/downloaddatabase").methods("GET"_method)([](crow::request& req, crow::response& res) {
+        // Open the database file
+        std::ifstream file(DATABASE_PATH, std::ios::binary);
+        if (!file.is_open()) {
+            res.code = 404;
+            res.body = "Database file not found";
+            res.end();
+            return;
+        }
+        
+        // Read the file contents
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        
+        // Set headers for file download
+        res.add_header("Content-Type", "application/octet-stream");
+        res.add_header("Content-Disposition", "attachment; filename=\"database.db\"");
+        res.add_header("Access-Control-Allow-Origin", "*");
+        
+        // Send the file
+        res.body = buffer.str();
+        res.end();
+    });
+
     CROW_ROUTE(app,"/getUsers").methods("GET"_method)([](crow::request& req, crow::response& res){
         auto data=get_users_from_db();
         res.set_header("Content-Type", "application/json");
