b81ca1b33ef2dd1be61e3b245a8c98285ee53a1d
IleaBogdan <ilea.bogdan06@gmail.com>
2026-03-09 20:26:25 +0200

    moved includes to separat .h file

---
+++ libs.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <crow.h>
+#include <random>
+#include <sqlite3.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "json.hpp"
+using json=nlohmann::json;
